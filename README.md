89b9ba752745ef4d4e63e099f96c67fe282f614e
Thiago Hernandes de Souza <hernandes_sp@yahoo.com.br>
2018-03-24 00:00:48 -0300

    Parte inicial de configuração

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lab-shift-ordem-servico-db
 Shift - Ordem de Serviço - Back-end
+
+@author: Thiago Hernandes de Souza
+@since: 23-03-2018
---
+++ src/main/java/shift/com/br/ShiftMainApp.java
@@ -0,0 +1,12 @@
+package shift.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftMainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftMainApp.class,args);
+	}
+}
